ca134c86610f5f49292b8492e0b0886b96ba666d
sjojon <90796535+sjojon@users.noreply.github.com>
2023-03-30 14:29:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let file = '';
+
+function view() {
+    document.getElementById('app').innerHTML = /*html*/ `
+    <input type="file" id="fileInput" onchange="input(this)"><hr>
+    <div>
+    <img height="100px" width="100px" id="image" src="${file}" alt="Profilbilde">
+    </div>
+    `;
+} view();
+
+
+function input(fileInput) {
+    let file = fileInput.files[0];
+    let reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+        file = reader.result;
+        document.getElementById('image').setAttribute('src', file);
+    });
+
+    reader.readAsDataURL(file);
+    view();
+}
+
+// Bilder hentet fra https://randomuser.me/
+
+
+// ORIGINAL:
+// fileInput.addEventListener('change', e => {
+//     const file = fileInput.files[0];
+//     const reader = new FileReader();
+
+//     reader.addEventListener('load', () => {
+//         console.log(reader.result);
+//     });
+
+//     reader.readAsDataURL(file);
+// });
